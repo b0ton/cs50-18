1c088bb269b4b9bb1ba3e7508be5d414a3d9c362
b0ton <46270213+b0ton@users.noreply.github.com>
2019-01-02 04:51:16 +0200

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+
+int main(void)
+{
+    int t = 0;
+
+    int x = get_int("x: ");
+    while (x < 0 || x > 23)
+    {
+        x = get_int("x: ");
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < x - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+    for (int j = 0; j <= i + 1; j++)
+        {
+            printf("#");
+        }
+
+    printf("\n");
+    }
+}
