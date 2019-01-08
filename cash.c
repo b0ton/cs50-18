22106c88ec2305155fddfb1d16841fd0d9e5c151
b0ton <46270213+b0ton@users.noreply.github.com>
2019-01-08 10:02:52 +0200

    Update cash.c

--- cash.c
+++ cash.c
@@ -7,7 +7,7 @@ int main(void)
     int c = 0;
     float x = get_float("x: ");
 
-    while ( x < 0)
+    while (x < 0)
     {
         x = get_float("x: ");
     }
@@ -19,19 +19,19 @@ int main(void)
         x = (int)x % 25;
     }
 
-        if ((x / 10) > 0)
+    if ((x / 10) > 0)
     {
         c = c + (x / 10);
         x = (int)x % 10;
     }
 
-        if ((x / 5) > 0)
+    if ((x / 5) > 0)
     {
         c = c + (x / 5);
         x = (int)x % 5;
     }
 
-        if (x > 0)
+    if (x > 0)
     {
         c = c + x;
     }
