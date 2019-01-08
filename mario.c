874bcb74fa7b3eacb88b4ca17bc917f7b4945dbf
b0ton <46270213+b0ton@users.noreply.github.com>
2019-01-08 12:18:08 +0200

    Update mario.c

--- mario.c
+++ mario.c
@@ -6,13 +6,14 @@
 int main(void)
 {
     int t = 0;
-
-    int x = get_int("x: ");
-    while (x < 0 || x > 23)
+    int x;
+    
+    do
     {
         x = get_int("x: ");
     }
-
+    while (x < 0 || x > 23)
+        
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < x - i - 1; j++)
